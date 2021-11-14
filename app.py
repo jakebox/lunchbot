619757484685617ea1ad70d13e48f539c9c2d9f1
Jake B <jacobboxerman@gmail.com>
2021-11-14 17:11:55 -0600

    Moved read parsed text to app.py

--- app.py
+++ app.py
@@ -5,13 +5,21 @@ from twilio.twiml.messaging_response import Message, MessagingResponse
 import sys
 from datetime import date
 
-from lunch_parser import read_parsed_text_menu
+# from lunch_parser import read_parsed_text_menu
 
 days_of_the_week = ["M", "T", "W", "Th", "F"]
 
 app = Flask(__name__)
 
 today = date.today().strftime('%A')
+
+remote_menu_url = "https://fwp-lunchbot.s3.us-east-2.amazonaws.com/menus/menu.txt"
+
+def read_parsed_text_menu(): # Get contents of menu file
+    "Read contents of menu file."
+    remote_menu = requests.get(remote_menu_url)
+    remote_menu.encoding = remote_menu.apparent_encoding
+    return(remote_menu.text.strip())
 
 @app.route('/sms', methods=['POST'])
 def sms():
