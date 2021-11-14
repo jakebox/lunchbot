fb488c2bdd11bac37023e6a708779700f1634aa2
Jake B <jacobboxerman@gmail.com>
2021-11-14 16:59:14 -0600

    Lunchbot now gives whole menu at once.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from twilio.twiml.messaging_response import Message, MessagingResponse
 import sys
 from datetime import date
 
-from lunch_parser import read_menu
+from lunch_parser import read_parsed_text_menu
 
 days_of_the_week = ["M", "T", "W", "Th", "F"]
 
@@ -20,31 +20,22 @@ def sms():
     message_body = request.form['Body'].title().strip()
     resp = MessagingResponse()
 
-    if message_body in days_of_the_week: # If the message is something we know how to do
-        todays_menu = read_menu(message_body)
-    elif today[0] in days_of_the_week or today[0:1] in days_of_the_week: # Tuesday and Thursday
-        if today == "Thursday":
-            todays_menu = read_menu("Th")
-        else: # If someone sends something random, give them today's menu (or Tuesday if it's Thu... TODO)
-            todays_menu = read_menu(today[0])
-    else:
-        todays_menu = read_menu("M")
-    resp.message(todays_menu + '\n \nSponsored by CTC/Student Government & FTC Robotics')
+    resp.message(read_parsed_text_menu(True))
 
     return str(resp)
 
 if __name__ == '__main__':
-    # app.run()
-    message_body = sys.argv[1]
+    app.run()
+    # message_body = sys.argv[1]
 
-    if message_body in days_of_the_week: # If the message is something we know how to do
-        todays_menu = read_menu(message_body)
-    elif today[0] in days_of_the_week or today[0:1] in days_of_the_week: # Tuesday and Thursday
-        if today == "Thursday":
-            todays_menu = read_menu("Th")
-        else:
-            todays_menu = read_menu(today[0])
-    else:
-        todays_menu = read_menu("M")
-    print(todays_menu + '\n \nSponsored by CTC/Student Government & FTC Robotics')
+    # if message_body in days_of_the_week: # If the message is something we know how to do
+    #     todays_menu = read_menu(message_body)
+    # elif today[0] in days_of_the_week or today[0:1] in days_of_the_week: # Tuesday and Thursday
+    #     if today == "Thursday":
+    #         todays_menu = read_menu("Th")
+    #     else:
+    #         todays_menu = read_menu(today[0])
+    # else:
+    #     todays_menu = read_menu("M")
+    # print(todays_menu + '\n \nSponsored by CTC/Student Government & FTC Robotics')
     
--- lunch_parser.py
+++ lunch_parser.py
@@ -1,39 +1,47 @@
-# Grabs lunch menu and saves each day's menu to a file under menus/
-# To be run once weekly (Sunday evening I suppose)
+# Grabs lunch menu and saves it to a file. Combines other two .py files.
+# To be run once weekly (Sunday evening I suppose).
 
 from lunch_menu_parser import Lunchbot
 from lunch_menu_url_scraper import Menu
 
 from datetime import date
 import os
-from urllib import request
+import requests
+
+days_of_the_week = ["M", "T", "W", "Th", "F"]
+local_menu_file = "menu.txt"
 
 def get_menus():
-    menu = Menu()
+    "Take menu from Internet and save as plaintext file menu.txt."
 
-    url = menu.get_pdf_url()
+    menu = Menu() # lunch_menu_url_scraper.py
 
-    bot = Lunchbot(url)
+    url = menu.get_pdf_url() # Scrape and return URL of lunch menu
 
-    today = date.today().strftime('%A')
-
-    days_of_the_week = ["M", "T", "W", "Th", "F"]
-
-    # Check whether the specified path exists or not
-    if not os.path.exists('./menus/'):
-      os.makedirs('./menus/')
-
-    for day in days_of_the_week:
-        with open('menus/' + day + '.txt', 'w') as file:
-
-            file.write(bot.get_day(day))
+    bot = Lunchbot(url) # lunch_menu_parser.py, parse PDF from given URL
 
 
-def read_menu(day):
-    with open("menus/" + day + ".txt") as file:
-        todays_menu = file.read()
-        return todays_menu
+    with open(local_menu_file, 'w') as file: # Write each day's menu to file
+        for day in days_of_the_week:
+            file.write(bot.get_day(day) + '\n\n')
+
+
+remote_menu_url = "https://fwp-lunchbot.s3.us-east-2.amazonaws.com/menus/menu.txt"
+
+def read_parsed_text_menu(remote=False): # Get contents of menu file
+    "Read contents of menu file."
+    if remote:
+        remote_menu = requests.get(remote_menu_url)
+        remote_menu.encoding = remote_menu.apparent_encoding
+        return(remote_menu.text.strip())
+    else:
+        with open(local_menu_file) as file:
+            menu = file.read()
+            return menu.strip() # Return full menu text with no extra whitespace at bottom
 
 
 if __name__ == '__main__':
-  print(read_menu("M"))
+    # print("Scraping, parsing, and saving menu.")
+    # get_menus()
+    print("Reading from remote url", remote_menu_url + "\n")
+    print(read_parsed_text_menu(True))
